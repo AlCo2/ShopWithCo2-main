f6fdd8f334c33eba59445b3bd2ae8b7c7da18cff
AlCo2 <abdou.lahboub.wwe@gmail.com>
2022-08-09 19:30:28 +0000

    added javascript and some litle cool edit

---
+++ Co2Shop.js
@@ -0,0 +1,7 @@
+
+let items = document.getElementById('items')
+let button = document.getElementById('topdeals');
+
+button.onclick = function(){
+    window.scrollTo({behavior:'smooth',top:680});
+}
